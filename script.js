673de137d82304006a7be34f9a27b8a1f546858a
Mohamed Shams <68266191+Abo4ams@users.noreply.github.com>
2022-08-23 18:34:01 +0200

    Phase 3 done

--- script.js
+++ script.js
@@ -17,16 +17,16 @@ function stars(rating) {
 
 
 const populate = async (topic) => {
-    const respone = await fetch(`https://udemy-json-server.herokuapp.com/allTopics/`);
-    const data = await respone.json();
-    coursesJSON = data[0];
-    populateCourses(data[0], topic);
+    const response = await fetch(`https://udemy-json-server.herokuapp.com/allTopics/`);
+    const data = await response.json();
+    coursesJSON = data;
+    populateCourses(data, topic);
     return data;
 }
 
 function populateCourses(data, topic) {
     coursesJSON = data;
-    data = data[topic][0];
+    data = data[topic];
     const htmlCourseString = data.courses.map((course, index) => {
         return index == 0 ? `<div class="carousel-item active col-md-3 course ${course.id}">
                     <div class="img-wrapper"><img src="${course.image}" alt="course image" width="255.2px" height="145px"/></div>
@@ -75,8 +75,9 @@ function populateCourses(data, topic) {
 
 
 function populateFilteredCourses(data) {
-    const htmlString = data.map((course, index) => {
-        return index == 0 ? `<div class="carousel-item active col-md-3 course ${course.id}">
+    const htmlString = data.map((courses, index) => {
+        return courses.map((course, index2) => {
+        return  index == 0 && index2 == 0 ? `<div class="carousel-item active col-md-3 course ${course.id}">
                     <div class="img-wrapper"><img src="${course.image}" alt="course image" width="255.2px" height="145px"/></div>
                     <div class="course-content">
                         <div class="inline-blck"><h4><a href="#" class="fnt-md">${course.title}</a></h3></div>
@@ -94,19 +95,32 @@ function populateFilteredCourses(data) {
                         <div class="price fnt-md">E£${course.price}</div>
                     </div>
                 </div>`
-})}
+}).join("")}).join("")
+    document.querySelector('.carousel-inner').innerHTML = htmlString;
+    let skills = document.querySelector('.skills-banner');
+    skills.innerHTML = `<div class="skills-banner-text">
+    <h1 class="fnt-h-xl">Showing the search results:</h1></div>`;
+    skills.style = 'margin-bottom: 0'
+
+}
 
 (async () => await populate("python"))();
+
+allTopics.forEach(topic =>{
+    const click = document.getElementById(topic) ; 
+    click.addEventListener("click" , () => populateCourses(coursesJSON, topic))})
 
 
 let input = document.getElementById("srch-input");
 
 input.addEventListener('keyup', (e) =>{
     const searchString = e.target.value.toLowerCase();
-    const filteredCourses = coursesJSON.python.filter(course => {
-        return course.title.toLowerCase().includes(searchString);
-    });
-    populateCourses(filteredCourses);
+    let filteredCourses = [];
+    for(let i = 0; i < allTopics.length; i++){
+        let curr = coursesJSON[allTopics[i]].courses;
+        filteredCourses.push(Object.values(curr.filter(course => course.title.toLowerCase().includes(searchString))));
+    }
+    populateFilteredCourses(filteredCourses);
 });
 
 const myCarouselElement = document.querySelector('#myCarousel')
